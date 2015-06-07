e881d547e7d0384fc2b09faf989cc9c05813c3d9
Andrew Kreps <andrew.kreps@gmail.com>
2015-06-07 11:53:32 -0700

    Test-complete.

--- spec/lita/handlers/garfield_spec.rb
+++ spec/lita/handlers/garfield_spec.rb
@@ -2,11 +2,32 @@ require "spec_helper"
 
 describe Lita::Handlers::Garfield, lita_handler: true do
   it 'will return a random garfield comic' do
-    send '!garfield'
+    send_message '!garfield random'
     expect(last_replies).to contain('https://garfield.com/uploads/strips/')
   end
-  it 'will return today\'s garfield comic' do
-    
 
+  it 'will return a today\'s then a random garfield comic' do
+    send_message '!garfield'
+    expect(last_replies).to contain('https://garfield.com/uploads/strips/[today]')
+    send_message '!garfield'
+    expect(last_replies).to contain('https://garfield.com/uploads/strips/')
+  end
+
+  it 'will return today\'s garfield comic' do
+    send_message '!garfield today'
+    expect(last_replies).to contain('https://garfield.com/uploads/strips/[today]')
+  end
+
+  it 'will return the first garfield comic' do
+    send_message '!garfield first'
+    expect(last_replies).to contain('https://garfield.com/uploads/strips/[today]')
+  end
+
+  # Test the saved state of the last comic you requested.
+  it 'will return the first and then the next garfield comic' do
+    send_message '!garfield first'
+    expect(last_replies).to contain('https://garfield.com/uploads/strips/1978-06-19.jpg')
+    send_message '!garfield next'
+    expect(last_replies).to contain('https://garfield.com/uploads/strips/1978-06-20.jpg')
   end
 end
