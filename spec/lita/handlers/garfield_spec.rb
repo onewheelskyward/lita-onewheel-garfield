b32c60f42d6289e2a1f018e7fbcfa3a907899ee0
Andrew Kreps <andrew.kreps@gmail.com>
2015-06-29 21:08:01 -0700

    Command mode engaged.

--- spec/lita/handlers/garfield_spec.rb
+++ spec/lita/handlers/garfield_spec.rb
@@ -2,16 +2,16 @@ require 'spec_helper'
 
 describe Lita::Handlers::Garfield, lita_handler: true do
 
-  it { is_expected.to route('garfield') }
-  it { is_expected.to route('garfield random') }
-  it { is_expected.to route('garfield first') }
-  it { is_expected.to route('garfield last') }
-  it { is_expected.to route('garfield today') }
-  it { is_expected.to route('garfield 5/5/1998') }
-  it { is_expected.to route('garfield 5-5-1998') }
-  it { is_expected.to route('garfield 1998-5-5') }
-  it { is_expected.to route('garfield prev') }
-  it { is_expected.to route('garfield next') }
+  it { is_expected.to route_command('garfield') }
+  it { is_expected.to route_command('garfield random') }
+  it { is_expected.to route_command('garfield first') }
+  it { is_expected.to route_command('garfield last') }
+  it { is_expected.to route_command('garfield today') }
+  it { is_expected.to route_command('garfield 5/5/1998') }
+  it { is_expected.to route_command('garfield 5-5-1998') }
+  it { is_expected.to route_command('garfield 1998-5-5') }
+  it { is_expected.to route_command('garfield prev') }
+  it { is_expected.to route_command('garfield next') }
 
   def zero_prefix(dat)
     if dat.to_i < 10
@@ -27,54 +27,54 @@ describe Lita::Handlers::Garfield, lita_handler: true do
   end
 
   it 'will return a random garfield comic' do
-    send_message 'garfield random'
+    send_command 'garfield random'
     expect(replies.last).to include('https://garfield.com/uploads/strips/')
   end
 
   it 'will return a today\'s then a random garfield comic' do
-    send_message 'garfield'
+    send_command 'garfield'
     expect(replies.last).to include("https://garfield.com/uploads/strips/#{get_todays_image_filename}.jpg")
-    send_message 'garfield'
+    send_command 'garfield'
     expect(replies.last).to include('https://garfield.com/uploads/strips/')
   end
 
   it 'will return today\'s garfield comic' do
-    send_message 'garfield today'
+    send_command 'garfield today'
     expect(replies.last).to include("https://garfield.com/uploads/strips/#{get_todays_image_filename}.jpg")
   end
 
   it 'will return today\'s garfield comic' do
-    send_message 'garfield last'
+    send_command 'garfield last'
     expect(replies.last).to include("https://garfield.com/uploads/strips/#{get_todays_image_filename}.jpg")
   end
 
   it 'will return the first garfield comic' do
-    send_message 'garfield first'
+    send_command 'garfield first'
     expect(replies.last).to include('https://garfield.com/uploads/strips/1978-06-19.jpg')
   end
 
   it 'will return a garfield comic for a specific y-m-d date' do
-    send_message 'garfield 1998-5-5'
+    send_command 'garfield 1998-5-5'
     expect(replies.last).to include('https://garfield.com/uploads/strips/1998-05-05.jpg')
   end
 
   it 'will return a garfield comic for a specific m-d-y date' do
-    send_message 'garfield 5-5-1998'
+    send_command 'garfield 5-5-1998'
     expect(replies.last).to include('https://garfield.com/uploads/strips/1998-05-05.jpg')
   end
 
   it 'will return a garfield comic for a specific / date' do
-    send_message 'garfield 5/5/1998'
+    send_command 'garfield 5/5/1998'
     expect(replies.last).to include('https://garfield.com/uploads/strips/1998-05-05.jpg')
   end
 
   # Test the saved state of the last comic you requested.
   it 'will return the first and then the next and then the previous garfield comic' do
-    send_message 'garfield first'
+    send_command 'garfield first'
     expect(replies.last).to include('https://garfield.com/uploads/strips/1978-06-19.jpg')
-    send_message 'garfield next'
+    send_command 'garfield next'
     expect(replies.last).to include('https://garfield.com/uploads/strips/1978-06-20.jpg')
-    send_message 'garfield prev'
+    send_command 'garfield prev'
     expect(replies.last).to include('https://garfield.com/uploads/strips/1978-06-19.jpg')
   end
 
@@ -84,13 +84,13 @@ describe Lita::Handlers::Garfield, lita_handler: true do
     first = 'https://garfield.com/uploads/strips/1978-06-19.jpg'
     last = "https://garfield.com/uploads/strips/#{today.year}-#{zero_prefix today.month}-#{zero_prefix today.day}.jpg"
 
-    send_message 'garfield first'
+    send_command 'garfield first'
     expect(replies.last).to include(first)
-    send_message 'garfield prev'
+    send_command 'garfield prev'
     expect(replies.last).to include(first)
-    send_message 'garfield last'
+    send_command 'garfield last'
     expect(replies.last).to include(last)
-    send_message 'garfield next'
+    send_command 'garfield next'
     expect(replies.last).to include(last)
   end
 end
